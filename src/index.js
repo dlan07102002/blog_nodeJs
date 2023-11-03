7ddf6487d963d54a7cc654040284b910e55c2f85
Đức Lân <s.gintoki710@gmail.com>
2023-11-04 02:39:39 +0700

    main

--- src/index.js
+++ src/index.js
@@ -6,6 +6,10 @@ const app = express();
 const port = 3000;
 
 const route = require('./routes')
+const db = require('./config/db')
+
+//Connect db
+db.connect();
 
 app.use(express.static(path.join(__dirname, '/public')))
 
@@ -18,11 +22,11 @@ const hbs = create({
 //template engine
 app.engine('hbs', hbs.engine)
 app.set('view engine', 'hbs');
-app.set('views', path.join(__dirname, 'resources/views'))
+app.set('views', path.join(__dirname, 'resources', 'views'))
 
 //Route init
 route(app)
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`App listening on port ${port}`)
 })
